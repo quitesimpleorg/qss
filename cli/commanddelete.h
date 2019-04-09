6da823d7f36bc5dc4fa160901074a9ebf468a0a1
Albert S <mail@quitesimple.org>
2019-04-09 22:03:04 +0200

    CommandDelete: refactor: split handle to several methods

--- cli/commanddelete.h
+++ cli/commanddelete.h
@@ -8,6 +8,9 @@ public:
     using Command::Command;
 
     int handle(QStringList arguments) override;
+private:
+    int removeNonExistent(QSqlDatabase &db, bool verbose, bool dryRun, QString pattern);
+    int removePaths(const QStringList &paths, QSqlDatabase &db, bool verbose, bool dryRun);
 };
 
 #endif // COMMANDDELETE_H
