d2712e5293d82ea1229fa2c9d2dd27cfc47f9c7c
Albert S <mail@quitesimple.org>
2019-04-16 08:47:30 +0200

    Added databasefactory to create database connections

---
+++ cli/databasefactory.h
@@ -0,0 +1,16 @@
+#ifndef DATABASEFACTORY_H
+#define DATABASEFACTORY_H
+#include <QSqlDatabase>
+#include <QThreadStorage>
+#include "utils.h"
+class DatabaseFactory
+{
+private:
+    QString connectionString;
+public:
+    DatabaseFactory(QString connectionString);
+    QSqlDatabase createNew();
+    QSqlDatabase forCurrentThread();
+};
+
+#endif // DATABASEFACTORY_H
