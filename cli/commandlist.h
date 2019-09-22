f0ad31e138b8495c019de340fd396537d6018ae4
Albert S <mail@quitesimple.org>
2019-09-22 16:13:13 +0200

    list command current state

---
+++ cli/commandlist.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDSEARCH_H
+#define COMMANDSEARCH_H
+#include "command.h"
+#include "../shared/sqlitesearch.h"
+
+class CommandList : public Command
+{
+public:
+    using Command::Command;
+
+    int handle(QStringList arguments) override;
+
+};
+
+#endif // COMMANDSEARCH_H
